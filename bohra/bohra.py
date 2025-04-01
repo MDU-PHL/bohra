eac2ae1c61f64d65905970183b051ff076a74d2e
Kristy <kristyhoran15@gmail.com>
2025-04-01 12:22:44 +1100

    converting to click

--- bohra/bohra.py
+++ bohra/bohra.py
@@ -12,49 +12,179 @@ Bohra is modular allowing the user to choose between calling SNPs and generating
 
 
 # import logging
-import argparse
+# import argparse
+import click
 import pathlib
 import sys
 import os
 import shutil
 from bohra.SnpDetection import RunSnpDetection, SetupInputFiles, InitBohra, TestBohra
 from bohra.version import version
+from click.exceptions import UsageError
+from click._compat import get_text_stderr
 
 
-def run_pipeline(args):
-    '''
-    Run the pipeline for the first time
-    '''
+
+def _show_usage_error(self, file=None):
+    if file is None:
+        file = get_text_stderr()
+    color = None
+    if self.ctx is not None:
+        color = self.ctx.color
+        click.echo(self.ctx.get_help() + '\n', file=file, color=color)
+
+UsageError.show = _show_usage_error
+
+
+@click.group()
+def cli():
+    pass
+@cli.command()
+@click.option('--reads', '-r',
+              help='Path to reads file, which is a tab-delimited with 3 columns <isolatename>  <path_to_read1> <path_to_read2>.', 
+              default='',
+              type=click.Path(exists=True))
+@click.option('--contigs', '-c', 
+              help='Path to contigs file, which is a tab-delimited with 3 columns <isolatename>  <path_to_contigs>. OPTIONAL if you already have assemblies.', 
+              default='',
+              type=click.Path(exists=True))
+@click.option('--pipeline', '-p', 
+              default='preview',
+              help='The pipeline to run. `preview` - generates a rapid tree using mash distances | `default` - runs snippy, phylogenetic tree (if > 3 sequences), assemblies, mlst and amr gene detection | `all` - same as default but includes roary pangenome analysis', 
+              type=click.Choice(['preview', 'default', 'full', 'snps', 'phylogeny', 'assemble', 'amr_typing']))
+@click.option('--reference', '-ref', 
+              help='Path to reference (.gbk or .fa)', 
+              default='')
+@click.option('--mask', '-m', 
+              default='', 
+              help='Path to mask file if used (.bed)')
+@click.option('--abritamr_args', 
+              default="", 
+              help="Set if you would like to use point mutations, please provide a valid species.", 
+              type=click.Choice(['Neisseria', 'Acinetobacter_baumannii', "Campylobacter", "Enterococcus_faecalis", "Enterococcus_faecium", "Escherichia", "Klebsiella", "Salmonella", "Staphylococcus_aureus", "Staphylococcus_pseudintermedius", "Streptococcus_agalactiae", "Streptococcus_pneumoniae", "Streptococcus_pyogenes", "Vibrio_cholerae"]))
+@click.option('--kraken_db', '-k',
+              default=os.getenv("KRAKEN2_DEFAULT_DB", ''),
+              metavar='KRAKEN2_DEFAULT_DB',
+              show_default=True,
+              type=click.Path(exists=True), 
+              help="Path to DB for use with kraken2, if no DB present speciation will not be performed.")
+@click.option('--assembler', '-a',
+              default='shovill', 
+              help='Assembler to use (shovill uses spades > 3.14 with --isolate mode).',
+              type=click.Choice(['shovill', 'skesa', 'spades']))
+@click.option('--spades_args',
+              default="", 
+              help="Use to add arguments to spades (when running with --assembler spades) for example: '--cov-cutoff auto' ")
+@click.option('--cpus',
+              help='Number of max CPU cores to run, will define how many rules are run at a time, if 0 then the avail cpus will be determined at time of launch', 
+              default=0)
+@click.option('--minmap', '-mp',
+              help='Snippy - minimum read mapping quality to consider.', 
+              default='60')
+@click.option('--basequal', '-bq',
+              help='Snippy - Minimum base quality to consider.', 
+              default='13')
+@click.option('--minqual', '-mq',
+              help='Snippy - minumum QUALITY in VCF column 6', 
+              default='100')
+@click.option('--minfrac', '-mf',
+              help='Snippy - minumum proportion for variant evidence ', 
+              default='0')
+@click.option('--mincov', '-mc',
+              help='Snippy - minimum site depth to for calling alleles.', 
+              default='10')
+@click.option('--workdir', '-w',
+              default=pathlib.Path.cwd().absolute(), 
+              help='The directory where Bohra will be run, default is current directory', 
+              type=click.Path(exists=True))
+@click.option('--no_phylo',
+              is_flag=True, 
+              help='Set if you do NOT want to generate a phylogentic tree.')
+@click.option('--config','-cfg',
+              default = f"", 
+              help='An additional config file, required if running on a non-local machine, ie slurm, cloud. For help see documentation at https://github.com/MDU-PHL/bohra or https://www.nextflow.io/docs/latest/executor.html',
+              type=click.Path(exists=True)) # don't need this
+@click.option('--profile',
+              default=f"", 
+              help='The resource profile to use. Defaults to local, if using an alternative config file, this calue should represent the name of a profile provided')
+@click.option('--conda_path',       
+              default=pathlib.Path(os.getenv('CONDA_PREFIX', '')), 
+              help='The path to where your pre-installed conda envs are stored, defaults to installing conda envs in your work directory. This can be provided in your profiles settings as well - it assumes you have pre-configured all of your conda environments for each process run by bohra, this is an advanced setting. Please take care.')
+@click.option('--blast_db',
+              default=f"{os.getenv('BLAST_DB', '')}", 
+              help='Path to the mlst blast_db, defaults to what is installed in the environment.', 
+              type=click.Path(exists=True))
+@click.option('--data_dir',     
+              default=f"{os.getenv('PUBMLST_DB','')}", 
+              help='Path to the mlst datadir, defaults to what is installed in the environment.', 
+              type=click.Path(exists=True))
+@click.option('--mlst_exclude','-me',
+              default=[], 
+              help='mlst schemes to exclude - multiple possible ie -me scheme1 -me scheme2 -me scheme3',
+              multiple=True)
+@click.option('--mobsuite_db',
+              default=f"{os.getenv('MOBSUITE_DB','')}", 
+              help='Path to the mobsuite_db, defaults to what is installed in the environment.', 
+              type=click.Path(exists=True))
+@click.option('--gubbins',
+              is_flag=True, 
+              help='Set to use gubbins for recombination correction.')
+@click.option('--keep',
+              default='N', 
+              type=click.Choice(['Y', 'N']), 
+              help='If you are rerunning bohra over an exisiting directory set --keep to \'Y\' to archive report files - otherwise previous report files will be removed.')
+@click.option('--proceed',
+              is_flag=True, 
+              help='If you would like to proceed straigt to the pipeline.')
+@click.option('--force', '-f',
+              is_flag=True, 
+              help='Add if you would like to force a complete restart of the pipeline. All previous logs will be lost.')
+@click.option('--no-conda',
+              is_flag=True, 
+              help='Set if you DO NOT WANT to use separate conda environments for each nextflow process.')
+@click.option('--check',
+              is_flag=True, 
+              help='Check that dependencies are installed correctly.')
+def run():
+    """
+    Launch the Bohra pipeline.
+    """
+    # run_pipeline(args)
+    click.echo("Launching Bohra pipeline...")
+# def run_pipeline(args):
+#     '''
+#     Run the pipeline for the first time
+#     '''
     
-    R = RunSnpDetection(args)
-    return(R.run_pipeline())
+#     R = RunSnpDetection(args)
+#     return(R.run_pipeline())
 
-def find_reads(args):
+# def find_reads(args):
 
-    S = SetupInputFiles(args)
-    S.find_reads()
+#     S = SetupInputFiles(args)
+#     S.find_reads()
 
-def init_bohra():
+# def init_bohra():
     
-    I = InitBohra()
-    I.init()
+#     I = InitBohra()
+#     I.init()
 
-def test_bohra(args):
+# def test_bohra(args):
     
-    S = TestBohra(args)
-    S.run_tests()
-    # run_pipeline(inputs)
+#     S = TestBohra(args)
+#     S.run_tests()
+#     # run_pipeline(inputs)
 
-def main():
-    # setup the parser
-    # Check if CONDA_PREFIX is set
-    conda_var = os.getenv('CONDA_PREFIX')
-    if conda_var is None:
-        # Set the default value
-        conda_prefix = 'NO CONDA INSTALLED'
-        # Set the default value
-    else:
-        conda_prefix = f"{pathlib.Path(os.getenv('CONDA_PREFIX')).parent}"
+# def main():
+#     # setup the parser
+#     # Check if CONDA_PREFIX is set
+#     conda_var = os.getenv('CONDA_PREFIX')
+#     if conda_var is None:
+#         # Set the default value
+#         conda_prefix = 'NO CONDA INSTALLED'
+#         # Set the default value
+#     else:
+#         conda_prefix = f"{pathlib.Path(os.getenv('CONDA_PREFIX')).parent}"
     
     # parser = argparse.ArgumentParser(description=f'Bohra - a bacterial genomics pipeline - version {version}',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + version)
@@ -134,5 +264,5 @@ def main():
     #     args.func(args)
 	
 if __name__ == '__main__':
-    main()
+    run()
 
