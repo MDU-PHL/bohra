87af06eae1352c4ed66c0c7f40798baac8dd851c
Kristy <kristyhoran15@gmail.com>
2025-07-24 13:17:46 +1000

    fixing test installation

--- bohra/launcher/TestBohra.py
+++ bohra/launcher/TestBohra.py
@@ -68,10 +68,10 @@ def run_tests():
         _download_reads_from_github(download_stub=download_stub, isolate_list=isolate_list)
         LOGGER.info(f"Reads have been downloaded to {read_path}.")
         LOGGER.info(f"Now generating the input file from the reads.")
-        find_data(reads = f"{read_path}",contigs="")
+        find_data(reads = f"{read_path}",contigs="",isolate_ids ="" )
         
     elif _check_test_data(path = read_path, isolate_list = isolate_list):
-        find_data(reads = f"{read_path}",contigs="")
+        find_data(reads = f"{read_path}",contigs="",isolate_ids ="" )
         
     cmd = f"bohra run full -i bohra_input.tsv -r {reference} --proceed Y"
     LOGGER.info(f"Now testing that the bohra installation has worked.")
