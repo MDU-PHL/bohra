4328c3a510650d3ec461731a821ad0a63f3e3bcb
Kristy <kristyhoran15@gmail.com>
2025-12-11 10:53:53 +1100

    added in optional cpu for test

--- bohra/commands/test/bohratest.py
+++ bohra/commands/test/bohratest.py
@@ -4,9 +4,12 @@ import os
 from bohra.launcher.TestBohra import run_tests
 
 @click.command()
-def test():
+@click.option('--cpus',
+              default=1,
+              help="Number of CPUs to use for testing Bohra installation.") 
+def test(cpus:int=1):
     """
     Check that bohra is installed correctly and runs as expected.
     """
     
-    run_tests()
+    run_tests(cpus=cpus)
