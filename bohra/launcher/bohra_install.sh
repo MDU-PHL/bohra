b3a2d3002b795c42bb07df9dd0b2a42d01ba50ed
Torsten Seemann <torsten.seemann@gmail.com>
2025-12-10 15:25:39 +1100

    Clean up install bash script - part 1

--- bohra/launcher/bohra_install.sh
+++ bohra/launcher/bohra_install.sh
@@ -1,31 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Creates conda environments required for bohra
+set -e
 
 BOHRA_CONDA_ENVS=$CONDA_PREFIX/conda_envs
 ENVS_FILES=$1
 INSTALL=$2
 FORCE_REINSTALL=$3
-# abort if any step fails
-set -e
+
+INSTALLER="conda"
+
 # resets to base env
-eval "$(conda shell.bash hook)"
-echo "Checking your conda version"
-condav=$(conda -V | grep -Eo '[0-9]+([.][0-9]+)')
-echo Version $condav
+eval "$($INSTALLER shell.bash hook)"
+$INSTALLER -V
 
 # check that mamba is installed
 echo Checking which package installer to use
-if [ -x "$(which mamba)" ]
-then 
-    echo "mamba is present and will be used to install dependencies" 
+if [ -x "$(which mamba)" ] ; then
     INSTALLER="mamba"
-#   return 0 
-else 
-    echo "mamba is not installed - will have to use conda - please be patient this may take longer than expected"
-    INSTALLER="conda" 
-#   return 1 
 fi
-
+echo "Using installer: $INSTALLER"
 
 # declare TOOLS=(mash assemblers seqtk seqkit trees prokka snippy mlst kraken2 mob_suite panaroo abritamr gubbins snpdists ectyper emmtyper kleborate lissero meningotype ngmaster stype tbtamr datasmryzr shigapass sonneitype ska2 cluster classify-pangenome fastp)
 declare -A TOOLS=(
@@ -64,41 +57,37 @@ function check_installation(){
 
 }
 
-function install_tool(){
-    
-    echo Running $INSTALLER env create -p $BOHRA_CONDA_ENVS/$1 -f $ENVS_FILES/$1.yml
-    time $INSTALLER env create -p $BOHRA_CONDA_ENVS/$1 -f $ENVS_FILES/$1.yml
-    echo "Will check $2 setup was successful"
-    checkinstalled=$(check_installation $BOHRA_CONDA_ENVS/$1 $1)
-    if [[ $checkinstalled -ne 0 ]] 
-        then
-            echo "There was an error installing $1. Please check the error messages above"
-            echo -e "\e[1mExiting now. Please contact the Bohra developers if you need help\e[0m"
-            exit 1
+# $1 = the name of the env/tool
+function install_tool() {
+    icmd="$INSTALLER env create -p $BOHRA_CONDA_ENVS/$1 -f $ENVS_FILES/$1.yml"
+    echo "Running: $icmd"
+    $icmd
+    if [[ $? -ne 0 ]]; then
+            echo "FAILED: $icmd"
+            exit 2
     else
         echo "$1 has been set up successfully"
     fi
 }
 
     
-for key in ${!TOOLS[@]};do
-    if [[ $FORCE_REINSTALL == "true" && $INSTALL == "install" ]]
-        then
+# MAIN LOOP OVER ALL ENVS
+    
+for key in ${!TOOLS[@]}; do
+    if [[ $FORCE_REINSTALL == "true" && $INSTALL == "install" ]] ; then
         echo "Removing existing environment for $key"
         echo "Running rm -rf $BOHRA_CONDA_ENVS/$key"
         rm -rf $BOHRA_CONDA_ENVS/$key
         echo "Will reinstall $key"
         install_tool $key
     else
-        echo Checking set up for $key environment
+        echo "Checking set up for $key environment"
         isinstalled=$(check_installation $BOHRA_CONDA_ENVS/$key $key)
         # echo "is installed :$isinstalled"
-        if [[ "$isinstalled" != 0 ]]
-            then
-            echo Looks like $key is not installed. 
-            if [[ "$INSTALL" == "install" ]]
-                then
-                echo Setting up $key environment now
+        if [[ "$isinstalled" != 0 ]] ; then
+            echo "Looks like $key is not installed."
+            if [[ "$INSTALL" == "install" ]]; then
+                echo "Setting up $key environment now"
                 install_tool $key
             else
                 echo -e "\e[1m$key is not installed\e[0m"
