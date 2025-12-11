54e217e0b391b3f227dafc613810b8d462fe6b1d
Kristy <kristyhoran15@gmail.com>
2025-12-11 11:52:07 +1100

    adding in support for installation of a single environment

--- bohra/commands/check_setup/check_setup.py
+++ bohra/commands/check_setup/check_setup.py
@@ -15,8 +15,13 @@ from bohra.launcher.CheckDeps import check_dependencies, _check_databases
               is_flag=True,
             #   default=False,
               help="Download and/or setup required databases.")
+@click.option('--tool',
+              # is_flag=True,
+              type=click.Choice(['all', 'torstyverse', 'seqquality', 'relationships','ectyper','mob_suite','panaroo','kleborate','stype','tamr','sonneitype','classify-pangenome'], case_sensitive=False),
+              default="all",
+              help="Install only a specific set of tools from a single environment. Should really only be used for development and/or testing purposes.")
 
-def check_deps(install_dbs:bool=False, setup_databases:bool=False):
+def check_deps(install_dbs:bool=False, setup_databases:bool=False, tool:str="all"):
     """
     Check dependencies for Bohra - Highly recommended to run this before running the pipeline.
     """
@@ -25,5 +30,5 @@ def check_deps(install_dbs:bool=False, setup_databases:bool=False):
     print("Please be patient.")
     print("Will now check and install anything that may be required. ")
     # print(str(force_reinstall).lower())
-    check_dependencies(check = "check", force_reinstall='false' )
+    check_dependencies(check = "check", force_reinstall='false', tool=tool )
     _check_databases(db_install=install_dbs or setup_databases)
